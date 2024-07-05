f074ce3a6a6ad6101f2568918b7662bbcb0af390
Stephanie Gonzalez <gstephaniegonz@gmail.com>
2024-07-05 12:14:34 -0500

    agregando caracteres especiales

--- includes/database.php
+++ includes/database.php
@@ -3,11 +3,11 @@
 //database connection
 $db = mysqli_connect(
     $_ENV['DB_HOST'] ?? '',
-    $_ENV['DB_USER'] ?? '', 
-    $_ENV['DB_PASS'] ?? '', 
+    $_ENV['DB_USER'] ?? '',
+    $_ENV['DB_PASS'] ?? '',
     $_ENV['DB_NAME'] ?? ''
 );
-
+$db->set_charset("utf8mb4");
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
     echo "errno de depuración: " . mysqli_connect_errno();
